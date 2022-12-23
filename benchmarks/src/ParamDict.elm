b8cf65465aecbfbd6218ea4cdb292c35597250da
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-12-23 08:18:39 +0100

    Codegen ParamDict

---
+++ benchmarks/src/ParamDict.elm
@@ -0,0 +1,1 @@
+../../generated/ParamDict.elm
--- codegen/Generate.elm
+++ codegen/Generate.elm
@@ -23,4 +23,37 @@ main =
         , CustomDict.withKey (Gen.Maybe.annotation_.maybe Type.int)
             |> CustomDict.withToComparable (Gen.Maybe.withDefault (Elm.int 0))
             |> CustomDict.generate
+        , CustomDict.withKey (Type.named [] "Param")
+            |> CustomDict.withToComparable (\param -> Elm.apply (Elm.val "toComparable") [ param ] |> Elm.withType (Type.named [] "Key"))
+            |> CustomDict.withAdditionalDeclarations
+                [ Elm.alias "Param" paramAnnotation |> Elm.exposeWith { exposeConstructor = False, group = Just "Types" }
+                , Elm.alias "Ratio" (Type.tuple Type.int Type.int) |> Elm.exposeWith { exposeConstructor = False, group = Just "Types" }
+                , Elm.alias "Key" keyAnnotation
+                , Elm.fn ( "param", Just (Type.named [] "Param") )
+                    (\param ->
+                        Elm.triple
+                            (Elm.get "section" param)
+                            (Elm.get "ratio" param)
+                            (Elm.get "function" param)
+                    )
+                    |> Elm.withType (Type.function [ Type.named [] "Param" ] keyAnnotation)
+                    |> Elm.declaration "toComparable"
+                ]
+            |> CustomDict.generate
+        ]
+
+
+keyAnnotation : Type.Annotation
+keyAnnotation =
+    Type.triple Type.string (Type.named [] "Ratio") Type.string
+
+
+paramAnnotation : Type.Annotation
+paramAnnotation =
+    Type.record
+        [ ( "section", Type.string )
+        , ( "ratio", Type.named [] "Ratio" )
+        , ( "function", Type.string )
+        , ( "color", Type.named [ "Color" ] "Color" )
+        , ( "op", Type.function [ Type.int ] (Type.named [ "Benchmark", "LowLevel" ] "Operation") )
         ]
